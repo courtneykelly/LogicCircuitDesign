d5ccf5627bf32557a1d9b559f7110196f3eb3300
Eric Krebs <ekrebs@nd.edu>
2016-04-02 16:35:03 -0400

    new file:   backEnd/Block.h 	new file:   backEnd/Gate.h 	new file:   backEnd/Input.h 	new file:   backEnd/Wire.h 	new file:   backEnd/main.cpp

---
+++ backEnd/Wire.h
@@ -0,0 +1,51 @@
+#ifndef WIRE_H
+#define WIRE_H
+
+#include <iostream>   // for using cout
+#include "Block.h" // for pointers to objects
+	#include "Input.h"
+	#include "Gate.h"
+
+using namespace std;
+
+class Wire
+{
+	public:
+		Wire(Block*);	// constructor
+		~Wire();   // deconstructor
+		int getValue();
+		
+		void setPointer(Block* blockPtr);
+		
+
+	private:
+		Block* blockPtr;
+		int value;
+};
+
+
+// constructor
+Wire::Wire(Block* ptr)
+{
+	blockPtr = ptr;
+}
+
+
+// destructor
+Wire::~Wire()
+{
+
+}
+
+int Wire::getValue()
+{
+	return (blockPtr->getValue());
+}
+
+void Wire::setPointer(Block* ptr)
+{
+	blockPtr = ptr;
+}
+
+
+#endif
