fe6836e03f8ef11eff5f85dec020aafdb38d81e4
Eric Krebs <ekrebs@nd.edu>
2016-04-11 14:49:24 -0400

    modified:   main.cpp

--- eric/Window/main.cpp
+++ eric/Window/main.cpp
@@ -7,7 +7,7 @@
 // test the SDL window
 
 #include "Window.h"
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 
 
@@ -16,30 +16,32 @@ int main()
 {
     Window screen;
 
-    bool quit = false;
+	bool quit = false;
 
-    SDL_Event e;
+	SDL_Event e;
 
-    while (!quit)
-    {
-	// drawing functions go here!
-
-
-
-    
-	screen.draw(); // draws, delays, then clears
-
-	while (SDL_PollEvent( &e ) != 0 )
+	while (!quit)
 	{
-	    if (e.type == SDL_QUIT )
-	    {
-		quit = true;
-	    }
+		// drawing functions go here!
+
+
+
+
+		screen.draw(); // draws, delays, then clears
+
+		while (SDL_PollEvent( &e ) != 0 )
+		{
+			switch(e.type)
+			{
+				case SDL_QUIT:
+					quit = true;
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					screen.drawLine();
+					break;
+			}
+		}
 	}
-    }
-
-
-
 
     return 0;
 }
