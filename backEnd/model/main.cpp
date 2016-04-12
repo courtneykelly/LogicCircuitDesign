185647a3d0c78632742aa71662a5c5ae9d5cdca7
Eric Krebs <ekrebs@nd.edu>
2016-04-11 23:16:38 -0400

    new file:   backEnd/model/Block.h 	new file:   backEnd/model/Gate.h 	new file:   backEnd/model/Input.h 	new file:   backEnd/model/Makefile 	new file:   backEnd/model/Window.h 	new file:   backEnd/model/Wire.h 	new file:   backEnd/model/main.cpp 	new file:   backEnd/model/project

---
+++ backEnd/model/main.cpp
@@ -0,0 +1,34 @@
+// logicCircuitDesign
+
+// cse20212, final project
+
+// main.cpp
+
+// test the SDL window
+
+#include "Window.h"
+#include <SDL.h>
+
+
+int main()
+{
+	Window screen;
+	bool quit = false;
+	SDL_Event e;
+
+	while (!quit)
+	{
+		// drawing functions go here!
+
+		screen.drawWires();
+
+		screen.draw(); // draws, delays, then clears
+
+		while (SDL_PollEvent( &e ) != 0 )
+		{
+			quit = screen.eventHandler(e);
+		}
+	}
+
+	return 0;
+}
