759cd1ecddf8fd4f34b1e3a5bca123f99b4e0418
Eric Krebs <ekrebs@nd.edu>
2016-04-15 16:18:06 -0400

    modified:   main.cpp

--- eric/Window/main.cpp
+++ eric/Window/main.cpp
@@ -12,7 +12,7 @@
 
 int main()
 {
-<<<<<<< HEAD
+
     Window screen;
 
     bool quit = false;
@@ -23,20 +23,17 @@ int main()
     while (!quit)
     {
 	// drawing functions go here!
-	screen.drawAndGate();      
-
 
     
 	screen.draw(); // draws, delays, then clears
 
 	while (SDL_PollEvent( &e ) != 0 )
-=======
+	
 	Window screen;
 	bool quit = false;
 	SDL_Event e;
 
 	while (!quit)
->>>>>>> 185647a3d0c78632742aa71662a5c5ae9d5cdca7
 	{
 		// drawing functions go here!
 
