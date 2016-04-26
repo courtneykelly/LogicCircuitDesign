1c8c00eec7737a0148e001652ca20a276f580dbb
Ben Dalgarn <bendalgarn@bens-air.dhcp.nd.edu>
2016-04-25 21:41:12 -0400

    Added getEquation functionality

---
+++ ben/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str1 = "hello ";
+    std::string str2 = "world";
+    std::string str3 = str1 + str2;
+
+    std::cout << str3 << std::endl;
+    
+    return 0;
+}
